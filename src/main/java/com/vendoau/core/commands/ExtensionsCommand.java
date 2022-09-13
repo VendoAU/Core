fef502e7a174879bee062543ce030d7f632c560f
VendoAU <reillyreaby@gmail.com>
2022-09-13 13:34:18 +1000

    Improved extensions command and added authors to extension.json

--- src/main/java/com/vendoau/core/commands/ExtensionsCommand.java
+++ src/main/java/com/vendoau/core/commands/ExtensionsCommand.java
@@ -1,7 +1,10 @@
 package com.vendoau.core.commands;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.extensions.Extension;
+import net.minestom.server.extensions.ExtensionDescriptor;
 import net.minestom.server.extensions.ExtensionManager;
 
 import java.util.ArrayList;
@@ -20,12 +23,33 @@ public class ExtensionsCommand extends CustomCommand {
             final Collection<Extension> extensions = extensionManager.getExtensions();
             sender.sendMessage("Extensions (" + extensions.size() + "):");
 
-            final List<String> extensionNames = new ArrayList<>();
-            extensions.forEach(extension -> {
-                extensionNames.add(extension.descriptor().name() + "(" + extension.descriptor().version() + ")");
-            });
+            final List<Component> extensionComponents = new ArrayList<>();
+            for (Extension extension : extensions) {
+                final ExtensionDescriptor descriptor = extension.descriptor();
+                final String name = descriptor.name();
+                final String version = descriptor.version();
+                final List<String> authors = descriptor.authors();
 
-            sender.sendMessage(String.join(", ", extensionNames));
+                Component hoverComponent = Component.text(name)
+                        .append(Component.newline())
+                        .append(Component.text("Version: " + version));
+                if (authors.size() == 1) {
+                    hoverComponent = hoverComponent
+                            .append(Component.newline())
+                            .append(Component.text("Author: " + authors.get(0)));
+                } else if (authors.size() != 0) {
+                    hoverComponent = hoverComponent
+                            .append(Component.newline())
+                            .append(Component.text("Authors: " + String.join(", ", authors)));
+                }
+
+                final Component extensionComponent = Component.text(name)
+                        .hoverEvent(hoverComponent);
+
+                extensionComponents.add(extensionComponent);
+            }
+
+            sender.sendMessage(Component.join(JoinConfiguration.commas(true), extensionComponents));
         });
     }
 }
